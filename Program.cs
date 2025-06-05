7168614926572f0613003403834c79be98ebe08c
veeramani <veera761998@gmail.com>
2025-06-05 12:34:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASIC_FUNCTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int AGE;
+            Console.WriteLine("Enter your age: ");  
+            AGE=int.Parse(Console.ReadLine());
+
+            string NAME;
+            Console.WriteLine("Enter your name: ");
+            NAME = Console.ReadLine();
+            Console.WriteLine("Hello " + NAME + ", you are " + AGE + " years old.");
+        }
+    }
+}
